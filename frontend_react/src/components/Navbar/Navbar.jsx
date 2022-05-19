c6da8fa85223f2dbb7cada1f5038cdbc8dafbc4a
Sebastian Martinez <84688005+SebastianMartinezA@users.noreply.github.com>
2022-05-18 22:56:34 -0300

    Fix flashing navbar when on mobile

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { useState } from 'react';
 import { motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 
 import { images } from '../../constants';
 import './Navbar.scss';
@@ -27,19 +28,22 @@ const [toggle, setToggle] = useState(false);
             <HiMenuAlt4 onClick={() => setToggle(true)}/>
             {
               toggle && (
+                
                 <motion.div
-                  whileInView={{ x: [300, 0]}}
-                  transition={{ duration: 0.85, ease: 'easeOut' }}
+                  initial={{ x: -600 }}
+                  animate={{ x: [300, 0]}}
                 >
                   <HiX onClick={() => setToggle(false)}/>
-                    <ul>
-                      {['home', 'about', 'work', 'testimonials', 'skills', 'contact'].map((item) => (
-                        <li key={item}>
-                          <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-                        </li>
-                      ))}
-                    </ul>
+                  <ul>
+                    {['home', 'about', 'work', 'testimonials', 'skills', 'contact'].map((item) => (
+                      <li key={item}>
+                        <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                      </li>
+                    ))}
+                  </ul>
+                      
                 </motion.div>
+                
               )
             }
         </div>
