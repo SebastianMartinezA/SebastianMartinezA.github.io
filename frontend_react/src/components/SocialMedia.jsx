0ca3d0f1a0b5461ea3c257e1339c1ef0b771ac55
Sebastian Martinez <84688005+SebastianMartinezA@users.noreply.github.com>
2022-05-25 23:05:30 -0300

    Change social links and add hrefs

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { BsTwitter , BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsLinkedin, BsGithub, BsDownload } from 'react-icons/bs';
+import resume from '../assets/ResumeSebastian.pdf';
+
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-        <div>
-            <BsTwitter />
-        </div>
-        <div>
-            <FaFacebookF />
-        </div>
-        <div>
-            <BsInstagram />
-        </div>
+        <a href="https://www.linkedin.com/in/smartinezamaray/" target="_blank" rel="noopener noreferrer">
+          <div>
+              <BsLinkedin />
+          </div>
+        </a>
+        <a href="https://github.com/SebastianMartinezA" target="_blank" rel="noopener noreferrer">
+          <div>
+              <BsGithub />
+          </div>
+        </a>
+        <a href={resume} download="CV Sebastian Martinez Amaray.pdf">
+          <div>
+              <BsDownload/>
+          </div>
+        </a>
     </div>
   )
 }
